262f13542e1d98cba50ea8dc011ca904d6312642
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-13 16:28:53 +0300

    add logic with submit and onchange

--- state-components/src/components/SearcInput.js
+++ state-components/src/components/SearcInput.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 
 export default class SearcInput extends Component {
+  state = {
+    imageTitle: ""
+  };
+
+  onCahgeInput = e => {
+    this.setState({ imageTitle: e.target.value });
+  };
+
+  onSubmitForm = e => {
+    console.log(this.state.imageTitle);
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="search card card-body mt-5">
         <h1>Search image</h1>
-        <form>
+        <form onSubmit={this.onSubmitForm}>
           <div className="row">
             <div className="col-sm-9">
               <input
+                onChange={this.onCahgeInput}
+                value={this.state.imageTitle}
                 type="text"
                 className="form-control"
                 placeholder="Movie title"
